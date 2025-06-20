30d44d0c8f882aa733c30c74b4e078961fe1ab29
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 15:32:35 +0800

    feat: add base api project

---
+++ springboot-gallo/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/SpringbootGalloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crhistianm.springboot.gallo.springboot_gallo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGalloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
